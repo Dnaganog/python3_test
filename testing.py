9f3a9458af952a1de1ff22f907ffaa7dd8242455
Daniel Nagano-Gerace <dnaganog@Daniels-MacBook-Pro.local>
2019-09-15 18:08:56 -0400

    works

--- testing.py
+++ testing.py
@@ -1,9 +1,9 @@
 def num_plus(num = 8, result = 0):
     if(num == 0): return result
     num += 1
-    return num_plus(num - 3, result + num)
+    return num_plus(num - 2, result + num)
 
 print(num_plus())
-print(num_plus())
-print(num_plus())
-print(num_plus())
+print(num_plus(10))
+print(num_plus(100))
+print(num_plus(500))
